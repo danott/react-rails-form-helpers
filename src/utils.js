b89ce8dbf75e1536f092572f88ddff036ef6d1f0
Dan Ott <thedanielott@gmail.com>
2017-08-29 13:54:35 -0400

    cleanup(PropTypes): use standalone PropTypes package
    Prepare for React 16

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-import React, { PropTypes } from "react"
-
+import React from "react"
+import PropTypes from "prop-types"
 
 export const nameWithContext = (Lower, prop = "name") => {
   const getDisplayName = (component) => component.displayName || component.name || "Component"
